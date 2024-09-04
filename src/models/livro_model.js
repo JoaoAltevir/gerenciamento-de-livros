1921c2e51f9b40d34da3f50b8628e5ff56eaa90b
João Neto <j.altevirneto@gmail.com>
2024-09-04 08:55:21 -0300

    feat: criado modelo de validação do livro

---
+++ src/models/livro_model.js
@@ -0,0 +1,24 @@
+let nextID = 1;
+
+const model = (body, id = nextID++) => {
+    if (
+        body.titulo != "" &&
+        body.titulo != undefined &&
+        body.autor != "" &&
+        body.autor != undefined &&
+        body.genero != "" &&
+        body.genero != undefined &&
+        body.ano != undefined
+    ) {
+        return {
+            id,
+            titulo: body.titulo,
+            autor: body.autor,
+            genero: body.genero,
+            ano: body.ano
+        }
+    }
+    return
+}
+
+module.exports = model;
