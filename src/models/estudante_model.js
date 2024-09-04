9dc77d446f8c2aad060702b5f52d43d115a10ddb
João Neto <j.altevirneto@gmail.com>
2024-09-04 09:30:55 -0300

    feat: criado modelo de validação de estudante

---
+++ src/models/estudante_model.js
@@ -0,0 +1,24 @@
+let nextID = 1;
+
+const model = (body, id = nextID++) => {
+    if (
+        body.nome != "" &&
+        body.nome != undefined &&
+        body.matricula != "" &&
+        body.matricula != undefined &&
+        body.curso != "" &&
+        body.curso != undefined &&
+        body.ano != undefined
+    ) {
+        return {
+            id,
+            nome: body.nome,
+            matricula: body.matricula,
+            curso: body.curso,
+            ano: body.ano
+        }
+    }
+    return
+}
+
+module.exports = model;
